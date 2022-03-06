47086092cdd6d9071a29ac4b8ccb6ef175e538da
Amin Baghbanbashi <amin_bghb78@yahoo.com>
2022-03-05 18:31:37 -0800

    Poll in Order Page Added

--- Orders/orders_js.js
+++ Orders/orders_js.js
@@ -10,6 +10,27 @@ $(document).ready(function(){
 
   $('.ui.checkbox').checkbox();
 
+  $('.ui.rating').rating({
+    initialRating: 2,
+    maxRating: 5
+  });
 
+  $(".poll_btn").click(function(){
+    $('#rating_modal').modal('show');
+  });
+
+  $(".submit_btn").click(function(){
+    var rate1 = $('.star1').rating('get rating');
+    $("input[name='rate1']").val(rate1);
+
+    var rate2 = $('.star2').rating('get rating');
+    $("input[name='rate2']").val(rate2);
+
+    var rate3 = $('.star3').rating('get rating');
+    $("input[name='rate3']").val(rate3);
+
+    // $('#rate_form').submit();
+    console.log($('#rate_form').serializeArray());
+  });
   
 });
