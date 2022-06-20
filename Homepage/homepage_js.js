bae08fa653ca3e5a82138f7adb018a50333a7f97
Amin Baghbanbashi <amin_bghb78@yahoo.com>
2022-06-20 21:27:31 +0430

    Footer and User Image Size Changed

--- Homepage/homepage_js.js
+++ Homepage/homepage_js.js
@@ -15,4 +15,16 @@ $(document).ready(function(){
         $('.selected_fixed').toggleClass('selected_fixed_ease_50', $(this).scrollTop() > $nav.height());
     });
 
+    $(".introduction_div .column_introduce .button").click(function(){
+        $('#introduce_modal').modal('show');
+    });
+
+    $(".introduction_div .column_goals .button").click(function(){
+        $('#goals_modal').modal('show');
+    });
+
+    $(".introduction_div .column_catalog .button").click(function(){
+        $('#catalog_modal').modal('show');
+    });
+
 });
